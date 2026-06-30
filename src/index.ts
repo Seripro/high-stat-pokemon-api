d746084060ad9a1ede1497600757533d3ecbf804
keigo <2364138keigo@gmail.com>
2026-06-30 14:58:54 +0900

    エンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { highStatUseCase } from "./container.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/high-stat", async (c) => {
+  const highStatPokemons = await highStatUseCase.execute();
+  return c.json(
+    highStatPokemons.map((pokemon) => ({
+      id: pokemon.id,
+      name: pokemon.name,
+      total_stat: pokemon.total_stat,
+    })),
+  );
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
